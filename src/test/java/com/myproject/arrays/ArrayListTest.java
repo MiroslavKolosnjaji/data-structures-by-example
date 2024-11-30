0cd43616609ee13ee8ad5f2e8b8ba78e89f0a6c0
Miroslav <kolosnjaji@stcable.net>
2024-11-30 17:34:29 +0100

    Added ArrayListTest class with one empty method just to initialize jacoco report.

---
+++ src/test/java/com/myproject/arrays/ArrayListTest.java
@@ -0,0 +1,15 @@
+package com.myproject.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Miroslav Kološnjaji
+ */
+class ArrayListTest {
+
+    @Test
+    void name() {
+    }
+}
