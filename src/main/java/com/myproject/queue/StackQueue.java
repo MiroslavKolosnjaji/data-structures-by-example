14a578865fdd60292c6152de24ee36e47ffb3c57
Miroslav <kolosnjaji@stcable.net>
2024-12-11 12:10:56 +0100

    Refactored 'StackQueue' methods.

--- src/main/java/com/myproject/queue/StackQueue.java
+++ src/main/java/com/myproject/queue/StackQueue.java
@@ -8,31 +8,62 @@ import java.util.Stack;
  */
 public class StackQueue<E> {
 
-    private Stack<E> firstStack = new Stack<>();
-    private Stack<E> secondStack = new Stack<>();
+    private final Stack<E> firstStack = new Stack<>();
+    private final Stack<E> secondStack = new Stack<>();
 
     public void add(E element) {
         firstStack.add(element);
     }
 
     public E remove() {
-        if (secondStack.isEmpty() && !firstStack.isEmpty()) {
-            moveElements();
-        }
+
+        moveElements();
+
         return secondStack.pop();
     }
 
+    public int size(){
+        return firstStack.size() + secondStack.size();
+    }
+
+    public boolean isEmpty(){
+        return firstStack.isEmpty() && secondStack.isEmpty();
+    }
+
     private void moveElements() {
-        while (!firstStack.isEmpty()) {
-            secondStack.push(firstStack.pop());
+        if (secondStack.isEmpty() && !firstStack.isEmpty()) {
+            while (!firstStack.isEmpty()) {
+                secondStack.push(firstStack.pop());
+            }
         }
+    }
+
+    private String getItems(boolean populated, Stack<E> stack){
+
+        moveElements();
+
+        Stack<E> tempStack = populated ? stack : new Stack<>();
+
+        while(!secondStack.isEmpty())
+            tempStack.push(secondStack.pop());
+
+        if(!firstStack.isEmpty())
+            getItems(true, tempStack);
+
+        String toString = tempStack.toString();
+
+        while (!tempStack.isEmpty())
+                secondStack.push(tempStack.pop());
+
+        return toString;
+    }
+
+    private String getItems(){
+        return getItems(false, null);
     }
 
     @Override
     public String toString() {
-
-        if (secondStack.isEmpty() && !firstStack.isEmpty())
-            moveElements();
-        return secondStack.toString();
+        return getItems();
     }
 }
